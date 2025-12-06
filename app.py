fb42553448148dccc387be9d25077837e6e986a7
QIUYX <qiuyx1218@outlook.com>
2025-12-06 07:19:05 +0000

    =

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3
+import datetime
 
 app = Flask(__name__)
 
@@ -8,14 +10,41 @@ def index():
 
 @app.route("/main", methods={"GET", "POST"})
 def main():
-    q = request.form.get("q")
-    print(q)
+    name = request.form.get("q")
+    timestamp = datetime.datetime.now()
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO user (name, timestamp) VALUES (?, ?)', (name, timestamp))
+    conn.commit()
+    c.close()
+    conn.close()
     return(render_template("main.html"))    
 
 @app.route("/paynow", methods={"GET", "POST"})
 def paynow():
     return(render_template("paynow.html"))    
 
+@app.route("/userlog", methods={"GET", "POST"})
+def userlog():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM user')
+    r = ""
+    for row in c:
+        r = r + str(row)
+    c.close()
+    conn.close()
+    return(render_template("userlog.html",r=r)) 
+
+@app.route("/deleteuserlog", methods={"GET", "POST"})
+def deleteuserlog():
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM user')
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("deleteuserlog.html")) 
 
 if __name__ == "__main__":
     app.run()
